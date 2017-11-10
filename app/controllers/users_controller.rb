1edc261f5026b497edd27855dc275f20793a4c01
Ruby Australia <rubyaustralia@localhost>
2017-11-10 22:15:19 +1000

    This should 404

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   private
 
   def user
-    @user ||= User.find_by_username(params[:username])
+    @user ||= User.find_by_username!(params[:username])
   end
   helper_method :user
 
